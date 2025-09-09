2f13b8fc71552549efb72fded847f12903cca0c9
driver <driver.studio.89@gmail.com>
2025-09-09 22:22:03 +0300

    implemented .env file for dev end production enviroment

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://my-vehicle-logger.onrender.com";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const login = async (email, password) => {
   const response = await fetch(`${API_BASE_URL}/auth/login`, {
